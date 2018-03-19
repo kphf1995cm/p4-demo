bab7c558c834087acab1622ac2f68838487bc115
kphf1995cm <kphf1995cm@outlook.com>
2018-03-19 02:16:04 -0700

    add register.p4

--- run_p4.sh
+++ run_p4.sh
@@ -1,5 +1,15 @@
-#! bin/bash
+#! /bin/bash
+
+# compile p4 to json by using p4c-bmv2 
+# run p4 using simple_switch
+
+
+p4_path=$1
+
+json_path="$1.json"
+
 simple_switch_path="/home/kp/user/behavioral-model/targets/simple_switch/simple_switch"
-json_path="gen_json.json"
-$simple_switch_path -i 0@if1 -i 1@if2 $json_path \
-   --thrift-port 9090
+
+p4c-bmv2 --json $json_path $p4_path
+
+$simple_switch_path -i 0@if1 -i 1@if2 $json_path --thrift-port 9090 
